89549ff846ca769a18ca475ba5126ed8ad2fd53e
Anurag Ranjan <anuran.ranjan@magicmindtechnologies.com>
2019-11-19 18:49:13 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -34,9 +34,7 @@ userLogin =  this.fb.group({
 
 
   onSubmit() {
-
-
-    const today = new Date();
+     const today = new Date();
     const date = today.getFullYear() +'-'+ (today.getMonth() + 1) + '-' + today.getDate();
     const time = today.getHours() + ":" + today.getMinutes() + ':' + today.getSeconds();
     const dateTime = date +' '+ time;
